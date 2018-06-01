965777c3fd228924877303c78326a4ff1afedb17
maxwellharon <maxwellharon@gmail.com>
2018-06-01 23:28:34 +0300

    Add url api key in the git call method

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -10,4 +10,10 @@ export class ProfileService {
   private clientid = 'e6e35541af8025357b25';
   private clientsecret = '12ed9835a3a925077e6bd2ac967d71e2ada7345c';
   constructor(private http:Http) { }
+  console.log("Service is now ready!")
+  this.username = 'maxwellharon';
+}
+
+getProfileInfo(){
+  return this.http.get(" https://api.github.com/users/maxwellharon ")
 }

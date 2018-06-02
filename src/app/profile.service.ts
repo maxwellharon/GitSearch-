e5f5ab32ea5321bb0153fc0c6444f67bda5b0da5
maxwellharon <maxwellharon@gmail.com>
2018-06-03 02:45:20 +0300

    Form is now linked to github and it works

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -22,4 +22,8 @@ export class ProfileService {
     .map(res =>res.json());
 
   }
+  updateProfile(username:string){
+    this.username = username;
+
+  }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,11 +9,17 @@ import { ProfileService } from './profile.service';
 export class ProfileComponent implements OnInit {
   profile: any[];
   repos: any [];
+  username:string;
 
 
   constructor(private profileService:ProfileService) {
+
+   }
+   findProfile(){
+     this.profileService.updateProfile(this.username);
      this.profileService.getProfileInfo().subscribe(profile =>{
        console.log(profile);
+       this.profile = profile;
      });
      this.profileService.getProfileRepos().subscribe(repos =>{
        console.log(repos);
